dc75bd544e807b7e38f2e31b6b13efbca00088b5
phongpham2909 <phongpham2140051@gmail.com>
2018-08-23 16:48:56 +0700

    Update File 23/08/2018 Lan1

--- src/Components/ProductDetail.js
+++ src/Components/ProductDetail.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import queryString from 'query-string';
 import DataUtils from '../Util/DataUtils';
-import { Grid, Row, Col, Image, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Grid, Row, Col, Image, ListGroup, ListGroupItem, Button } from 'react-bootstrap';
 import '../Custom_Style/ProductDetail.css';
 
 import CurrencyFormat from 'react-currency-format';
@@ -10,14 +10,14 @@ class ProductDetail extends Component {
     constructor(props) {
         super(props);
         this.state = { Id: null, info: null }
-
+        
         this.state.Id = this.getIdFromQueryString();
         this.state.info = this.getProduct();
     }
 
     getProduct() {
         let product = null;
-        if (this.state.Id != null) {
+        if (this.state.Id !== null) {
             product = DataUtils.getProduct(this.state.Id);
         }
         return product;
@@ -25,7 +25,6 @@ class ProductDetail extends Component {
     buildProductImage() {
         let imageTemplate = [];
         if (this.state.info !== null) {
-            console.log(this.state.info.img)
             imageTemplate.push(
                 <div key={1}>
                     <Image src={this.state.info.img} responsive />
@@ -42,7 +41,7 @@ class ProductDetail extends Component {
             let salePriceTemp = (<CurrencyFormat value={salePrice} displayType={'text'} thousandSeparator={true} suffix={' vnđ'} />)
             descriptionTemplate.push(
                 <div key={1}>
-                    <ListGroup>
+                    <ListGroup className="list-group-detail">
                         <ListGroupItem header={this.state.info.name}><span>Thương Hiệu: {this.state.info.company}</span></ListGroupItem>
                         <ListGroupItem header='GIÁ SẢN PHẨM'>
                             <span>
@@ -54,10 +53,8 @@ class ProductDetail extends Component {
                                         suffix={' vnđ'}
                                         renderText={value => <span> {value}</span>} /></strong>
                             </span>
-
                             <br />
                             <span><strong>Giảm giá:</strong> {this.state.info.saleoff}% </span>
-
                             <span>
                                 ({salePriceTemp})
                            </span>
@@ -66,27 +63,99 @@ class ProductDetail extends Component {
                                 <CurrencyFormat value={this.state.info.price} displayType={'text'} thousandSeparator={true} suffix={' vnđ'} />
                             </span>
                         </ListGroupItem>
-                        <ListGroupItem header=''>
-                        <span dangerouslySetInnerHTML={{__html:this.state.info.description}}></span>
-                        </ListGroupItem>
                     </ListGroup>
+                    <Button className="btn-add-to-card">Mua Ngay</Button>
                 </div>
             )
         }
         return descriptionTemplate;
     }
 
+    buildProductDescriptionShipping() {
+        let productDescriptionShipping = [];
+        if (this.state.info != null) {
+            productDescriptionShipping.push(
+                <div key={1}>
+
+                    <Row className="line-detail-des">
+                        <Col md={12}>
+                            <h4>MÔ TẢ SẢN PHẨM</h4>
+                            <span dangerouslySetInnerHTML={{ __html: this.state.info.description }}></span>
+                        </Col>
+                    </Row>
+                </div>
+            )
+        }
+        return productDescriptionShipping;
+    }
+
     buildProductDetail() {
         let productDetail = [];
-        let product = this.getProduct();
-        if (product != null) {
+        //let product = this.getProduct();
+        if (this.state.info != null) {
             productDetail.push(
                 <div key={1}>
-                    <span>Name: {product.name}</span>
-                    <br />
-                    <span>Price: {product.price}</span>
-                    <br />
-                    <span>Description: {product.description}</span>
+                    <Row className="line-detail">
+                        <Col md={12}>
+                            <h4>THÔNG SỐ KỸ THUẬT</h4>
+                            <span className="text-label">Đơn vị lưu kho</span>
+                            <span className="data">{this.state.info.donviluukho}</span>
+                        </Col>
+                    </Row>
+                    <Row className="line-detail">
+                        <Col md={12}>
+                            <span className="text-label">Độ dày mặt</span>
+                            <span className="data">{this.state.info.dodaymat}</span>
+                        </Col>
+                    </Row>
+                    <Row className="line-detail">
+                        <Col md={12}>
+                            <span className="text-label">Màu mặt</span>
+                            <span className="data">{this.state.info.colorface}</span>
+                        </Col>
+                    </Row>
+                    <Row className="line-detail">
+                        <Col md={12}>
+                            <span className="text-label">Cấu tạo máy</span>
+                            <span className="data">{this.state.info.cautaomay}</span>
+                        </Col>
+                    </Row>
+                    <Row className="line-detail">
+                        <Col md={12}>
+                            <span className="text-label">Chiều rộng dây</span>
+                            <span className="data">{this.state.info.chieurongday}</span>
+                        </Col>
+                    </Row>
+                    <Row className="line-detail">
+                        <Col md={12}>
+                            <span className="text-label">Độ dài có thể điều chỉnh</span>
+                            <span className="data">{this.state.info.dodaicothedieuchinh}</span>
+                        </Col>
+                    </Row>
+                    <Row className="line-detail">
+                        <Col md={12}>
+                            <span className="text-label">Dây</span>
+                            <span className="data">{this.state.info.day}</span>
+                        </Col>
+                    </Row>
+                    <Row className="line-detail">
+                        <Col md={12}>
+                            <span className="text-label">Màu dây</span>
+                            <span className="data">{this.state.info.mauday}</span>
+                        </Col>
+                    </Row>
+                    <Row className="line-detail">
+                        <Col md={12}>
+                            <span className="text-label">Dây đồng hồ thay thế được</span>
+                            <span className="data">{this.state.info.daydonghothaythe}</span>
+                        </Col>
+                    </Row>
+                    <Row className="line-detail">
+                        <Col md={12}>
+                            <span className="text-label">Chống nước</span>
+                            <span className="data">{this.state.info.chongnuoc}</span>
+                        </Col>
+                    </Row>
                 </div>
             )
         }
@@ -100,33 +169,41 @@ class ProductDetail extends Component {
         }
         if (Obj.id) {
             Obj.id = parseInt(Obj.id);
-
         }
         return Obj.id || null;
     }
 
     render() {
         let imageTemplate = this.buildProductImage();
-        //  let productDetail = this.buildProductDetail();  
+        let productDetail = this.buildProductDetail();
         let descriptionTemplate = this.buildDescription();
+        let productDescriptionShipping = this.buildProductDescriptionShipping();
         return (
             <div>
-                <Grid>
-                    <Row className="product-detail" >
-                        <Col className="product-detail-img" xs={12} sm={12} md={6} >
-                            {imageTemplate}
+                <Grid fluid={true}>
+                    <Row className="product-detail-main">
+                        <Col xs={12} sm={12} md={12} lg={12}>
+                            <Row className="product-detail">
+                                <Col className="product-detail-img" xs={12} sm={12} md={6}>
+                                    {imageTemplate}
+                                </Col>
+                                <Col className="product-detail-des" xs={12} sm={12} md={6}>
+                                    {descriptionTemplate}
+                                </Col>
+                            </Row>
                         </Col>
-                        <Col className="product-detail-des" xs={12} sm={12} md={6}  >
-                            {descriptionTemplate}
-                        </Col>
-
                     </Row>
-                    <Row >
-                        <Col md={6} mdPush={6}>
-
-                        </Col>
-                        <Col md={6} mdPull={6}>
-
+                    <br />
+                    <Row className="product-description-main">
+                        <Col xs={12} sm={12} md={12} lg={12}>
+                            <Row className="product-description">
+                                <Col xs={12} sm={12} md={6}>
+                                    {productDescriptionShipping}
+                                </Col>
+                                <Col xs={12} sm={12} md={6}>
+                                    {productDetail}
+                                </Col>
+                            </Row>
                         </Col>
                     </Row>
                 </Grid>
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-import { Table, Panel } from 'react-bootstrap';
+import {Grid,Row,Col,Thumbnail,Button } from 'react-bootstrap';
 import DataUtils from '../Util/DataUtils';
 import $ from 'jquery';
+import '../Custom_Style/ProductManagerment.css'
+
+
+import CurrencyFormat from 'react-currency-format';
 
 class Products extends Component {
     constructor(props) {
@@ -15,19 +19,31 @@ class Products extends Component {
         event.preventDefault();
     }
 
+    /*<td>{parseInt(i) + 1}</td>
+    <td>{ProductList[i].name}</td>
+    <td>{ProductList[i].type}</td>
+    <td>{ProductList[i].price}</td>*/
     buildProductList() {
         let ProductList = DataUtils.getProductList();
         let _productlist = [];
-
         for (let i in ProductList) {
             _productlist.push(
-                <tr key={i} id={ProductList[i].id} onDoubleClick={this.onDetail}>
-                    <td>{parseInt(i) + 1}</td>
-                    <td>{ProductList[i].name}</td>
-                    <td>{ProductList[i].type}</td>
-                    <td>{ProductList[i].price}</td>
-
-                </tr>
+                <div key={i}>
+                    <Grid>
+                        <Row className="product-managerment">
+                            <Col xs={6} md={4}>
+                                <Thumbnail src={ProductList[i].img} alt="242x200">
+                                    <h3>{ProductList[i].name}</h3>
+                                    <p>{ProductList[i].company}</p>
+                                    <p> <CurrencyFormat value={ProductList[i].price} displayType={'text'} thousandSeparator={true} suffix={' vnđ'} /></p>
+                                    <p>
+                                        <Button className="btn-add-to-card" id={ProductList[i].id} onClick={this.onDetail}>Xem Ngay </Button>
+                                    </p>
+                                </Thumbnail>
+                            </Col>
+                        </Row>
+                    </Grid>
+                </div>
             )
         }
         return _productlist;
@@ -36,26 +52,10 @@ class Products extends Component {
     render() {
         let _productlist = this.buildProductList();
         return (
-            <div>
-                <Panel bsStyle="primary">
-                    <Panel.Heading>
-                        <Panel.Title componentClass="h3">Product Managerment</Panel.Title>
-                    </Panel.Heading>
-                    <Panel.Body><Table>
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Product Name</th>
-                                <th>Type</th>
-                                <th>Price</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {_productlist}
-                        </tbody>
-                    </Table></Panel.Body>
-                </Panel>
-
+            <div>        
+  
+            {_productlist}
+   
             </div>
         );
     }
